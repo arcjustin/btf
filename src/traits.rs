e919fb90483b6547d0e62e8195d6cea18e0bcfce
Justin Deschamp <deschampjustin@gmail.com>
2022-10-29 00:16:29 +0000

    Define generic AddToBtf implementation for arrays

--- src/traits.rs
+++ src/traits.rs
@@ -262,220 +262,8 @@ impl AddToBtf for isize {
     }
 }
 
-impl<const N: usize> AddToBtf for [u8; N] {
-    /// Trait implementation for adding Rust's [u8; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[u8; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[u8; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("u8").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("u8").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        u8::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[u8; {}]", N),
-            "usize",
-            "u8",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [u16; N] {
-    /// Trait implementation for adding Rust's [u16; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[u16; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[u16; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("u16").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("u16").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        u16::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[u16; {}]", N),
-            "usize",
-            "u16",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [u32; N] {
-    /// Trait implementation for adding Rust's [u32; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[u32; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[u32; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("u32").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("u32").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        u32::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[u32; {}]", N),
-            "usize",
-            "u32",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [u64; N] {
-    /// Trait implementation for adding Rust's [u64; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[u64; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[u64; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("u64").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("u64").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        u64::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[u64; {}]", N),
-            "usize",
-            "u64",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [i8; N] {
-    /// Trait implementation for adding Rust's [i8; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[i8; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[i8; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("i8").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("i8").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        i8::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[i8; {}]", N),
-            "usize",
-            "i8",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [i16; N] {
-    /// Trait implementation for adding Rust's [i16; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[i16; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[i16; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("i16").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("i16").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        i16::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[i16; {}]", N),
-            "usize",
-            "i16",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [i32; N] {
-    /// Trait implementation for adding Rust's [i32; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[i32; N]".
-    ///
-    /// # Arguments
-    ///
-    /// * `btf` - The database to which the type is to be added.
-    ///
-    /// # Example
-    /// ```
-    /// use btf::{AddToBtf, BtfTypes, Type};
-    ///
-    /// let mut btf = BtfTypes::default();
-    /// <[i32; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("i32").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("i32").expect("New type can't be resolved.");
-    /// ```
-    fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
-        usize::add_to_btf(btf)?;
-        i32::add_to_btf(btf)?;
-        btf.add_array(
-            &format!("[i32; {}]", N),
-            "usize",
-            "i32",
-            N.try_into().expect("Array size too large"),
-        )
-    }
-}
-
-impl<const N: usize> AddToBtf for [i64; N] {
-    /// Trait implementation for adding Rust's [i64; N] type to an existing
-    /// BTF type database. This is added to the database as an array of 8-bit,
-    /// unsigned integers with the name "[i64; N]".
+impl<T: AddToBtf, const N: usize> AddToBtf for [T; N] {
+    /// Generic trait implemenation for adding arrays to a BTF type database.
     ///
     /// # Arguments
     ///
@@ -487,16 +275,16 @@ impl<const N: usize> AddToBtf for [i64; N] {
     ///
     /// let mut btf = BtfTypes::default();
     /// <[i64; 5]>::add_to_btf(&mut btf).expect("Failed to add type.");
-    /// btf.get_type_by_name("i64").expect("New type doesn't exist.");
-    /// btf.resolve_type_by_name("i64").expect("New type can't be resolved.");
+    /// btf.get_type_by_name("[i64; 5]").expect("New type doesn't exist.");
+    /// btf.resolve_type_by_name("[i64; 5]").expect("New type can't be resolved.");
     /// ```
     fn add_to_btf(btf: &mut BtfTypes) -> Option<&Type> {
         usize::add_to_btf(btf)?;
-        i64::add_to_btf(btf)?;
+        T::add_to_btf(btf)?;
         btf.add_array(
-            &format!("[i64; {}]", N),
+            &format!("[{}; {}]", std::any::type_name::<T>(), N),
             "usize",
-            "i64",
+            std::any::type_name::<T>(),
             N.try_into().expect("Array size too large"),
         )
     }
